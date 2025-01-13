7cf8471b749a603f82a132f14c31bb503928d3a7
Matt <casper.angelfire@gmail.com>
2025-01-13 07:26:13 -0600

    Learning dialogs

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.awt.event.*;
 public class Main {
     public static void main(String[] args) {
         MyApp app = new MyApp();
-        app.setSize(new Dimension(300, 300));
+        app.setSize(new Dimension(500, 500));
         app.setTitle("Menu Bars");
         app.setVisible(true);
     }
@@ -14,31 +14,23 @@ class MyApp extends Frame {
     MyApp() {
         setLayout(new FlowLayout());
 
-        MenuBar menuBar = new MenuBar();
+        Dialog dialog = new Dialog(this, "Hello World", false);
+        dialog.setSize(new Dimension(250, 250));
+        dialog.setLayout(new FlowLayout());
+        dialog.setVisible(true);
 
-        Menu section1 = new Menu("File");
-        MenuItem open = new MenuItem("Open");
-        open.setEnabled(false);
-        MenuItem save = new MenuItem("Save");
-        MenuItem close = new MenuItem("Close");
+        dialog.add(new Label("Press this button to close"));
 
-        section1.add(open).addActionListener(e -> System.out.println("Opening"));
-        section1.add(new MenuItem("-"));
-        section1.add(save).addActionListener(e -> System.out.println("Saving"));
-        section1.add(new MenuItem(""));
-        section1.add(close).addActionListener(e -> System.exit(0));
+        Button button = new Button("Close");
+        dialog.add(button);
+        button.addActionListener(e -> dialog.dispose());
 
-        Menu section2 = new Menu("Options");
-        MenuItem graphics = new MenuItem("Graphics");
-        CheckboxMenuItem oc = new CheckboxMenuItem("Occlusion Culling", true);
-
-        section2.add(graphics);
-        section2.add(oc);
-
-        menuBar.add(section1);
-        menuBar.add(section2);
-
-        setMenuBar(menuBar);
+        dialog.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                dialog.dispose();
+            }
+        });
 
         addWindowListener(new WindowAdapter() {
             @Override
