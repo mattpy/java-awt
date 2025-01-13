5f222c29cdea88712420a2eb4c3431298c2ba923
Matt <casper.angelfire@gmail.com>
2025-01-13 04:56:32 -0600

    Test button

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import java.awt.*;
+import java.awt.event.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        MyApp app = new MyApp();
+        app.setSize(new Dimension(300, 300));
+        app.setTitle("Java AWT");
+        app.setVisible(true);
+    }
+}
+
+class MyApp extends Frame {
+    private int count = 0;
+
+    MyApp() {
+        setLayout(new FlowLayout());
+
+        Button button = new Button("Click");
+        button.addActionListener((e) -> {
+            count++;
+            repaint();
+        });
+        add(button);
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        g.drawString(String.format("You clicked the button %d times", count), 20, 100);
     }
 }
