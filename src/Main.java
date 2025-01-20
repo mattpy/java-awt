4baaede67d4dc2c98a740df6260b15d2e952b9bb
Matt <casper.angelfire@gmail.com>
2025-01-20 10:11:55 -0600

    Create image tile randomizer

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,61 @@
+import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        MyApp app = new MyApp();
-        app.setSize(new Dimension(500, 500));
-        app.setTitle("Menu Bars");
-        app.setVisible(true);
+        App a = new App();
+
+        a.setSize(new Dimension(500, 500));
+        a.setTitle("Learn AWT");
+        a.setVisible(true);
     }
 }
 
-class MyApp extends Frame {
-    MyApp() {
+class App extends Frame {
+    private static final int NUM_COLS = 8;
+    private static final int NUM_ROWS = 4;
+
+    private final Image[] cell = new Image[NUM_ROWS * NUM_COLS];
+
+    private int tileWidth;
+    private int tileHeight;
+
+    App() {
         setLayout(new FlowLayout());
 
-        Dialog dialog = new Dialog(this, "Hello World", false);
-        dialog.setSize(new Dimension(250, 250));
-        dialog.setLayout(new FlowLayout());
-        dialog.setVisible(true);
+        try {
+            Image image = ImageIO.read(new File("image.png"));
 
-        dialog.add(new Label("Press this button to close"));
+            int imageWidth = image.getWidth(null);
+            int imageHeight = image.getHeight(null);
 
-        Button button = new Button("Close");
-        dialog.add(button);
-        button.addActionListener(e -> dialog.dispose());
+            tileWidth = imageWidth / NUM_COLS;
+            tileHeight = imageHeight / NUM_ROWS;
 
-        dialog.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                dialog.dispose();
+            for (int y = 0; y < NUM_ROWS; y++) {
+                for (int x = 0; x < NUM_COLS; x++) {
+                    CropImageFilter cif = new CropImageFilter(tileWidth * x, tileHeight * y, tileWidth, tileHeight);
+                    FilteredImageSource fis = new FilteredImageSource(image.getSource(), cif);
+                    int cellIndex = y * NUM_COLS + x;
+                    cell[cellIndex] = createImage(fis);
+                }
             }
+
+            randomizeCells();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Button randomizeButton = new Button("Randomize");
+        randomizeButton.addActionListener(e -> {
+            randomizeCells();
+            repaint();
         });
+        add(randomizeButton);
+
 
         addWindowListener(new WindowAdapter() {
             @Override
@@ -38,5 +63,36 @@ class MyApp extends Frame {
                 System.exit(0);
             }
         });
+    }
+
+    private void randomizeCells() {
+        int numCells = NUM_ROWS * NUM_COLS;
+        for (int i = 0; i < numCells * 2; i++) {
+            int randomCellOne = (int) (Math.random() * numCells);
+            int randomCellTwo = (int) (Math.random() * numCells);
+            Image temp = cell[randomCellOne];
+            cell[randomCellOne] = cell[randomCellTwo];
+            cell[randomCellTwo] = temp;
+        }
+    }
+
+    @Override
+    public void update(Graphics g) {
+        // Prevent automatic screen clearing
+        paint(g);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        for (int y = 0; y < NUM_ROWS; y++) {
+            for (int x = 0; x < NUM_COLS; x++) {
+                g.drawImage(
+                        cell[y * NUM_COLS + x],
+                        x * tileWidth + getInsets().left,
+                        y * tileHeight + getInsets().top,
+                        null
+                );
+            }
+        }
     }
 }
