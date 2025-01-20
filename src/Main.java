cd10e6d7298560e933512aafbf5a34d4dd1b0a19
Matt <casper.angelfire@gmail.com>
2025-01-20 13:18:49 -0600

    Move image randomizer to separate package

--- src/Main.java
+++ src/Main.java
@@ -1,98 +1,11 @@
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.event.*;
-import java.awt.image.*;
-import java.io.*;
+import imagedemos.*;
 
 public class Main {
     public static void main(String[] args) {
-        App a = new App();
+        CropImageFilterDemo a = new CropImageFilterDemo();
 
-        a.setSize(new Dimension(500, 500));
         a.setTitle("Learn AWT");
         a.setVisible(true);
     }
 }
 
-class App extends Frame {
-    private static final int NUM_COLS = 8;
-    private static final int NUM_ROWS = 4;
-
-    private final Image[] cell = new Image[NUM_ROWS * NUM_COLS];
-
-    private int tileWidth;
-    private int tileHeight;
-
-    App() {
-        setLayout(new FlowLayout());
-
-        try {
-            Image image = ImageIO.read(new File("image.png"));
-
-            int imageWidth = image.getWidth(null);
-            int imageHeight = image.getHeight(null);
-
-            tileWidth = imageWidth / NUM_COLS;
-            tileHeight = imageHeight / NUM_ROWS;
-
-            for (int y = 0; y < NUM_ROWS; y++) {
-                for (int x = 0; x < NUM_COLS; x++) {
-                    CropImageFilter cif = new CropImageFilter(tileWidth * x, tileHeight * y, tileWidth, tileHeight);
-                    FilteredImageSource fis = new FilteredImageSource(image.getSource(), cif);
-                    int cellIndex = y * NUM_COLS + x;
-                    cell[cellIndex] = createImage(fis);
-                }
-            }
-
-            randomizeCells();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        Button randomizeButton = new Button("Randomize");
-        randomizeButton.addActionListener(e -> {
-            randomizeCells();
-            repaint();
-        });
-        add(randomizeButton);
-
-
-        addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
-        });
-    }
-
-    private void randomizeCells() {
-        int numCells = NUM_ROWS * NUM_COLS;
-        for (int i = 0; i < numCells * 2; i++) {
-            int randomCellOne = (int) (Math.random() * numCells);
-            int randomCellTwo = (int) (Math.random() * numCells);
-            Image temp = cell[randomCellOne];
-            cell[randomCellOne] = cell[randomCellTwo];
-            cell[randomCellTwo] = temp;
-        }
-    }
-
-    @Override
-    public void update(Graphics g) {
-        // Prevent automatic screen clearing
-        paint(g);
-    }
-
-    @Override
-    public void paint(Graphics g) {
-        for (int y = 0; y < NUM_ROWS; y++) {
-            for (int x = 0; x < NUM_COLS; x++) {
-                g.drawImage(
-                        cell[y * NUM_COLS + x],
-                        x * tileWidth + getInsets().left,
-                        y * tileHeight + getInsets().top,
-                        null
-                );
-            }
-        }
-    }
-}
