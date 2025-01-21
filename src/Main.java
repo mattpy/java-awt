9161a4088a27ef5f90f7fc133414bb3ed1d17b59
Matt <casper.angelfire@gmail.com>
2025-01-20 18:54:28 -0600

    change filename

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        ImageFilterDemo a = new ImageFilterDemo("photo.jpgg");
+        ImageFilterDemo a = new ImageFilterDemo("photo.jpg");
 
         a.setSize(new Dimension(500, 500));
         a.setTitle("Learn AWT");
